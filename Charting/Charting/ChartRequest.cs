e1c3553764ec8194367386f7ea5950e3238b5c3f
Peter Kalivas <pkalivas@gmail.com>
2022-01-05 17:28:35 -0600

    commenting

---
+++ Charting/Charting/ChartRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Charting
+{
+    public class ChartRequest
+    {
+        public string Title { get; set; }
+        public string YOneName { get; set; }
+        public string YTwoName { get; set; }
+        public List<float> YOneData { get; set; }
+        public List<float> YTwoData { get; set; }
+    }
+}
---
+++ Charting/Charting/ChartingService.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using XPlot.Plotly;
+
+namespace Charting
+{
+
+    public static class ChartingService
+    {
+        public static PlotlyChart GetChart(ChartRequest request)
+        {
+            var prediction = new Scatter
+            {
+                name = request.YOneName,
+                x = request.YOneData.Select((_, idx) => idx).ToList(),
+                y = request.YOneData.Select(val => val).ToList(),
+            };
+
+            var actualValues = new Scatter
+            {
+                name = request.YTwoName,
+                x = request.YTwoData.Select((_, idx) => idx).ToList(),
+                y = request.YTwoData.Select(val => val).ToList(),
+                yaxis = "y2"
+            };
+
+            var layout = new Layout.Layout
+            {
+                title = request.Title,
+                yaxis = new Yaxis
+                {
+                    title = request.YOneName
+                },
+                yaxis2 = new Yaxis
+                {
+                    side = "right",
+                    overlaying = "y",
+                    title = request.YTwoName
+                }
+            };
+
+            var chart = Chart.Plot(new[] { prediction, actualValues });
+            chart.WithTitle("");
+            chart.WithLayout(layout);
+
+            return chart;
+        }
+    }
+}
